88d2f43753b288469607e8025a44501f1769f47a
LiadDn <liadd11222@gmail.com>
2026-05-27 22:08:34 +0300

    update docker compuse

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const client = require('prom-client');
 
-const server = http.createServer((req, res) => {
-  res.write('Hello DevOps!!!!!!');
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequests = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  registers: [register]
+});
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/metrics') {
+    res.setHeader('Content-Type', register.contentType);
+    res.end(await register.metrics());
+    return;
+  }
+  
+  httpRequests.inc();
+  res.write('Hello DevOps!');
   res.end();
 });
 
 server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
-
 
